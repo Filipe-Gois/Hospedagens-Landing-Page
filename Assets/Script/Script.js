162fa833bdec55be9582d9f497d66ccc453fd8ec
Filipe <fythoy@gmail.com>
2024-08-16 17:18:40 -0300

    .

--- Assets/Script/Script.js
+++ Assets/Script/Script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const menuHamburguer = document.querySelector(".hamburger");
+
+const menuHamburguerInput = document.querySelector("#input-hamburguer");
+
+const navBar = document.querySelector(".navbar");
+
+menuHamburguer.addEventListener("click", (e) => {
+  e.preventDefault();
+  menuHamburguerInput.checked = !menuHamburguerInput.checked;
+  navBar.classList.toggle("exibeNavBar");
+});
